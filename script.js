59eedffd5b4d28e677d697b1689f59dfc4133b8b
abdul-grow <abdul@grow.ae>
2022-11-30 16:53:06 +0400

    Initial Commit with Slider Code

---
+++ script.js
@@ -0,0 +1,68 @@
+var sliderContainer = document.querySelector('.slider-wrapper');
+var slider = document.querySelector('.each-slider');
+var leftNav = document.querySelector('.slider-nav__left');
+var rightNav = document.querySelector('.slider-nav__right');
+var sliderLength = document.querySelectorAll('.each-slider').length;
+var sliderDots = document.querySelector('.slider-dots');
+var index = 1;
+sliderContainer.dataset.index = index;
+
+rightNav.addEventListener('click', function(){
+    if(index < sliderLength){
+        sliderContainer.style.transform = 'translateX(-'+ (index)*100 +'%)';
+        index++;
+        sliderContainer.dataset.index = index;
+        setActiveDots(index);
+    }
+    else{
+        sliderContainer.style.transform = 'translateX(0%)';
+        index = 1;
+        setActiveDots(index);
+    }
+    
+});
+
+leftNav.addEventListener('click', function(){
+    if(index <= 1 ){
+        sliderContainer.style.transform = 'translateX(0%)';
+        index = 1;
+        setActiveDots(index);
+    }
+    else{
+        --index;
+        sliderContainer.dataset.index = index;
+        sliderContainer.style.transform = 'translateX(-'+ (index-1)*100 +'%)';
+        setActiveDots(index);
+    }
+});
+
+// Create dots
+for (i=0; i < sliderLength; i++){
+    var divDots = document.createElement('div'); 
+    divDots.classList.add('slider-dots__each');
+    if(i == 0){
+        divDots.classList.add("active");
+    }
+    sliderDots.append(divDots);
+}
+
+var sliderDotAll = document.querySelectorAll('.slider-dots__each');
+sliderDotAll.forEach(function(ele,ind){
+    ele.addEventListener('click',function(){
+        var dotIndex = ind + 1;
+        setActiveDots(dotIndex);
+        sliderContainer.style.transform = 'translateX(-'+ (dotIndex-1)*100 +'%)';
+        sliderContainer.dataset.index = dotIndex;
+        index = dotIndex;
+    })
+})
+
+function setActiveDots(index){
+    var sliderDotAll = document.querySelectorAll('.slider-dots__each');
+    sliderDotAll.forEach(function(eachEle,eachIndex){
+        eachEle.classList.remove('active');
+        if(index == eachIndex + 1 ){
+            eachEle.classList.add('active');
+        }
+    });
+}
